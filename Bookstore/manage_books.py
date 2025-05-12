3f89cdd4ce565f4816178f6f92c9580531d5551e
danilamalyshev <danikmalyshev@gmail.com>
2025-05-12 17:38:39 +0200

    modiffied by Danik

--- Bookstore/manage_books.py
+++ Bookstore/manage_books.py
@@ -6,22 +6,30 @@ book_format = ['ID', 'Title', 'Author', 'Year', 'Count', 'Modified']
 
 def add_book(new_book):
     with open("../ DATABASE/ book.csv", "r", encoding="utf-8") as file:
-        reader = list(csv.reader(file))
+        reader = list(csv.DictReader(file))
 
     reader.append(new_book)
 
     with open("../ DATABASE/ book.csv", "w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(book_format)
+        writer = csv.DictWriter(file, fieldnames=book_format)
+        writer.writeheader()
         writer.writerows(reader)
 
 
-new_id = generate_id()
+new_id = str(generate_id())
 new_book_title = input("Enter new book title: ")
 new_book_author = input("Enter new book author: ")
 new_book_year = input("Enter new book year: ")
 new_book_count = input("Enter new book count: ")
 new_book_modified = input("Enter new book modified: ")
 
-new_book_data = [new_id, new_book_title, new_book_author, new_book_year, new_book_count, new_book_modified]
-add_book(new_book_data)
+new_book = {
+    'ID': new_id,
+    'Title': new_book_title,
+    'Author': new_book_author,
+    'Year': new_book_year,
+    'Count': new_book_count,
+    'Modified': new_book_modified
+}
+
+add_book(new_book)
