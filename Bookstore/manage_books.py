d4859cfb6a71bceb19cc6b90a16415d27e173814
danilamalyshev <danikmalyshev@gmail.com>
2025-05-12 16:58:57 +0200

    modiffied by Danik

--- Bookstore/manage_books.py
+++ Bookstore/manage_books.py
@@ -1,12 +1,31 @@
-def add_book():
-    books = []
-    while True:
-        print("1 to escape")
-        a = input("Enter a book title: ")
-        books.append(a)
-        if a == "1":
-            books.pop()
-            break
+import csv
+from utils import generate_id
+
+book_format = ['ID', 'Title', 'Author', 'Year', 'Count', 'Modified']  # Добавил 'ID' как первый столбец
+
+def add_book(new_book):
+    with open("../ DATABASE/ book.csv", "r", encoding="utf-8") as file:
+        reader = list(csv.reader(file))
+
+    reader.append(new_book)
+
+    with open("../ DATABASE/ book.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(book_format)
+        writer.writerows(reader)
 
 
-add_book()
+new_id = generate_id()
+new_book_title = input("Enter new book title: ")
+new_book_author = input("Enter new book author: ")
+new_book_year = input("Enter new book year: ")
+new_book_count = input("Enter new book count: ")
+new_book_modified = input("Enter new book modified: ")
+
+new_book_data = [new_id, new_book_title, new_book_author, new_book_year, new_book_count, new_book_modified]
+add_book(new_book_data)
+
+#
+# def delete_book(books):
+#     info = input("Enter a book title or ID to delete: ")
+#     books.remove(info)
