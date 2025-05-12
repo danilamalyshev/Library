e7677cb3010bcd2ca6485e8b9fe7cd3e7b70c6c3
danik <danikmalyshev@gmail.com>
2025-05-12 14:00:43 +0200

    test1)

--- Bookstore/manage_customers.py
+++ Bookstore/manage_customers.py
@@ -3,7 +3,5 @@ import csv
 file_path = '../DATABASE/customer.csv'
 customers = []
 
-with open(file_path, mode="w", newline='', encoding='utf-8') as customersfile:
-    reader = csv.reader(customersfile)
-    for row in reader:
-        customers.append(input())
+with open(file_path, mode="w", encoding='utf-8') as customersfile:
+    print("111111")
--- Bookstore/utils.py
+++ Bookstore/utils.py
@@ -5,5 +5,7 @@ def get_ID():
     user_id = uuid.uuid4()
     return str(user_id)
 
+
 get_ID()
 
+print(get_ID())
